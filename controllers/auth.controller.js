f6b07f53f1227dac58fb26abb1d02343ef7b25d0
tchoforwilson <tchoforwilson@gmail.com>
2023-05-08 11:52:02 +0100

    Implemented method to send response with user token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,3 +10,26 @@ const signToken = (user) => {
     expiresIn: config.jwt.expiresIn,
   });
 };
+
+/**
+ * @breif Create and send as response jwt token
+ * @param {Object} user -> User object
+ * @param {statusCode} statusCode -> Response status code
+ * @param {req} Request
+ * @param {res} Response
+ */
+const createSendToken = (user, statusCode, res) => {
+  // 1. Get token
+  const token = signToken(user);
+
+  // 2. Remove user password from output
+  user.password = undefined;
+
+  // 3. Send response
+  res.status(statusCode).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+};
