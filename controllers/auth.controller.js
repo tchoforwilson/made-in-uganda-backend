9355d3debfeaefc1ab3f15a3733097c71a23a404
tchoforwilson <tchoforwilson@gmail.com>
2023-05-08 21:12:35 +0100

    Implement controller for password reset

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -192,6 +192,33 @@ const forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
+const resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 3) Update changedPasswordAt property for the user
+  // 4) Log the user in, send JWT
+  createSendToken(user, 200, res);
+});
+
 /**
  * @breif Controller for updating user password
  */
@@ -220,5 +247,6 @@ export default {
   protect,
   restrictTo,
   forgotPassword,
+  resetPassword,
   updatePassword,
 };
