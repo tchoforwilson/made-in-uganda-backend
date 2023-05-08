9ebae62993038cc38334db5c912fee6ef3c161f8
tchoforwilson <tchoforwilson@gmail.com>
2023-05-08 16:26:45 +0100

    Added route to update user password

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.post('/updateMyPassword', authController.updatePassword);
 
 // PROTECT ALL ROUTE
 router.use(authController.protect);
