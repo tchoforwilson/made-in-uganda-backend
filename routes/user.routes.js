b565db376a1ce007acfdc69884deb11534024267
tchoforwilson <tchoforwilson@gmail.com>
2023-05-08 21:15:24 +0100

    Added password reset route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,6 +7,7 @@ const router = Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
+router.post('/resetPassword/:token', authController.resetPassword);
 
 // PROTECT ALL ROUTE AFTER THIS MIDDLEWARE
 router.use(authController.protect);
