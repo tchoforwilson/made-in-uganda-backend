52f4503ce5ef43ebcc14682c135107b526eeb012
tchoforwilson <tchoforwilson@gmail.com>
2023-05-08 21:23:36 +0100

    Implemented utility method to filter out unwanted object fields

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,19 @@
 import User from '../models/user.model';
 
+/**
+ * @breif Filter out unwanted fields in an object
+ * @param {Object} obj -> Provided object
+ * @param  {...any} allowedFields -> Fields allowed to be updated
+ * @returns {Object}
+ */
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 const getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
