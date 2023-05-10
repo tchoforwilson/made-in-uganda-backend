26257f36ea7d54e07df3538684c0961f39dcca1d
tchoforwilson <tchoforwilson@gmail.com>
2023-05-10 11:01:56 +0100

    add product router to application

---
+++ app.js
@@ -0,0 +1,57 @@
+import express, { json, urlencoded } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
+import { fileURLToPath } from 'url';
+
+import config from './configurations/config.js';
+import globalErrorHandler from './controllers/error.controller.js';
+import AppError from './utilities/appError.js';
+import userRouter from './routes/user.routes.js';
+import productRouter from './routes/product.routes.js';
+
+// Start express app
+const app = express();
+
+// 1) GLOBAL MIDDLEWARES
+// Serving static files
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (config.env === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Body parser, reading data from body into req.body
+app.use(json({ limit: '10kb' }));
+app.use(urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use(limiter);
+
+// ROUTES
+app.use(`${config.prefix}/users`, userRouter);
+app.use(`${config.prefix}/products`, productRouter);
+
+// INVALID ROUTES
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// GLOBAL ERROR HANDLER
+app.use(globalErrorHandler);
+
+export default app;
