76a1e915407db1ada5600d14a51f5288a9446855
tchoforwilson <tchoforwilson@gmail.com>
2023-05-10 01:16:49 +0100

    Implement utility for file upload

---
+++ utilities/upload.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+export default upload;
