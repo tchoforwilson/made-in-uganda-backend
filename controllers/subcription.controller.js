aca336348e287c45020d417271c292d2e235a589
tchoforwilson <tchoforwilson@gmail.com>
2023-05-12 17:34:35 +0100

    Nested route for Subcription and store

--- controllers/subcription.controller.js
+++ controllers/subcription.controller.js
@@ -1,6 +1,12 @@
 import Subcription from '../models/subcription.model.js';
 import factory from './handler.factory.js';
 
+const setSubcriptionStoreId = (req, res, next) => {
+  // Allow for nested routes
+  if (!req.body.store) req.body.store = req.user.id || req.params.storeId;
+  next();
+};
+
 const createSubscription = factory.createOne(Subcription);
 const getSubcription = factory.getOne(Subcription);
 const getAllSubcriptions = factory.getAll(Subcription);
@@ -8,6 +14,7 @@ const updateSubcription = factory.updateOne(Subcription);
 const deleteSubcription = factory.deleteOne(Subcription);
 
 export default {
+  setSubcriptionStoreId,
   createSubscription,
   getSubcription,
   getAllSubcriptions,
