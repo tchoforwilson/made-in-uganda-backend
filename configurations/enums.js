094642b23cfe314487765e8fa843dbf100b97988
tchoforwilson <tchoforwilson@gmail.com>
2023-05-12 13:43:19 +0100

    Configuration of enumerations basically product categories and currencies

---
+++ configurations/enums.js
@@ -0,0 +1,20 @@
+/**
+ * @breif Enumeration of currencies
+ */
+export const currency = ['DOLLAR', 'EUR'];
+
+/**
+ * @breif Enumeration of product categories
+ */
+export const categories = [
+  'Electronics',
+  'Breads Sweets',
+  'Frozen Seafood',
+  'Wines & Alcoholic Drinks',
+  'Coffees and Teas',
+  'Milks and Diaries',
+  'Cloths and Wear',
+  'Shoes',
+  'Kitchen Utensils',
+  'House hold',
+];
