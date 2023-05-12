1c036480155562d42ab8390bd5b5c7782bfb20ca
tchoforwilson <tchoforwilson@gmail.com>
2023-05-12 17:43:18 +0100

    Implement and refactored routes

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,5 +1,36 @@
 import { Router } from 'express';
 
+import authController from '../controllers/auth.controller.js';
+import productController from '../controllers/product.controller.js';
+
 const router = Router({ mergeParams: true });
+
+router
+  .route('/')
+  .get(productController.getAllProducts)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    productController.uploadProductImages,
+    productController.resizeProductImages,
+    productController.setProductStoreId,
+    productController.createProduct
+  );
+
+router
+  .route('/:id')
+  .get(productController.getProduct)
+  .patch(
+    authController.protect,
+    authController.restrictTo('user'),
+    productController.uploadProductImages,
+    productController.resizeProductImages,
+    productController.updateProduct
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'user'),
+    productController.deleteProduct
+  );
 
 export default router;
