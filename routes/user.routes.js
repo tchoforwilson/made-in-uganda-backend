93d34e632331c66aacfce218e4b648f3cd8fc03d
tchoforwilson <tchoforwilson@gmail.com>
2023-05-12 14:10:19 +0100

    Added merge param for products and subcriptions

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import authController from '../controllers/auth.controller.js';
 import userController from '../controllers/user.controller.js';
+import productRouter from './product.routes.js';
+import subcriptionRouter from './subcription.routes.js';
 
 const router = Router();
 
@@ -9,6 +11,10 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword/:token', authController.resetPassword);
+
+// Merge param for shop products search
+router.use('/:storeId/products', productRouter);
+router.use('/:storeId/subcriptions', subcriptionRouter);
 
 // PROTECT ALL ROUTE AFTER THIS MIDDLEWARE
 router.use(authController.protect);
