ba7d2cf3c61bf962af379af51cfe9122a54e82a9
tchoforwilson <tchoforwilson@gmail.com>
2023-06-02 08:40:25 +0100

    Store model definition

---
+++ models/store.model.js
@@ -0,0 +1,59 @@
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
+
+const storeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  employees: {
+    type: Number,
+    required: [true, 'Please provide number of employees'],
+  },
+  telephone: {
+    type: String,
+    validate: [validator.isMobilePhone],
+  },
+  logo: String,
+  address: {
+    line_1: {
+      type: String,
+      required: [true, 'Please provide address line'],
+    },
+    line_2: String,
+    city: {
+      type: String,
+      required: [true, 'Please tell us the city'],
+    },
+    region: {
+      type: String,
+      required: [true, 'Provide the state/province/region'],
+    },
+    zipcode: {
+      type: String,
+      required: [true, 'Provide zipcode'],
+    },
+  },
+  location: {
+    // GeoJSON
+    type: {
+      type: String,
+      default: 'Point',
+      enum: ['Point'],
+    },
+    coordinates: [Number],
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Store must belong to an owner'],
+  },
+});
+
+const Store = model('Store', storeSchema);
+
+export default Store;
