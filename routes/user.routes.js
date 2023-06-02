0e47872e64d98a5d534b5c69ba474acdf0106d93
tchoforwilson <tchoforwilson@gmail.com>
2023-06-02 09:12:16 +0100

    Refactored

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 import authController from '../controllers/auth.controller.js';
 import userController from '../controllers/user.controller.js';
-import productRouter from './product.routes.js';
-import subcriptionRouter from './subcription.routes.js';
 
 const router = Router();
 
@@ -12,21 +10,12 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword/:token', authController.resetPassword);
 
-// Merge param for shop products search
-router.use('/:storeId/products', productRouter);
-router.use('/:storeId/subcriptions', subcriptionRouter);
-
 // PROTECT ALL ROUTE AFTER THIS MIDDLEWARE
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
-router.patch(
-  '/updateMe',
-  userController.uploadStorePhoto,
-  userController.resizeStorePhoto,
-  userController.updateMe
-);
+router.patch('/updateMe', userController.updateMe);
 router.patch('/deleteMe', userController.deleteMe);
 
 // Restrict all route after this to admin
