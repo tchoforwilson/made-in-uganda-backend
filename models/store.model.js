5e09afd4b05de7ab96bb6d590b5d3a3afa18c49a
tchoforwilson <tchoforwilson@gmail.com>
2023-06-07 11:41:53 +0100

    Included virtuals

--- models/store.model.js
+++ models/store.model.js
@@ -1,57 +1,74 @@
 import { Schema, model } from 'mongoose';
 import validator from 'validator';
 
-const storeSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  employees: {
-    type: Number,
-    required: [true, 'Please provide number of employees'],
-  },
-  telephone: {
-    type: String,
-    validate: [validator.isMobilePhone],
-  },
-  logo: String,
-  address: {
-    line_1: {
+const storeSchema = new Schema(
+  {
+    name: {
       type: String,
-      required: [true, 'Please provide address line'],
+      required: true,
     },
-    line_2: String,
-    city: {
+    description: {
       type: String,
-      required: [true, 'Please tell us the city'],
+      required: true,
     },
-    region: {
-      type: String,
-      required: [true, 'Provide the state/province/region'],
+    employees: {
+      type: Number,
+      required: [true, 'Please provide number of employees'],
     },
-    zipcode: {
+    telephone: {
       type: String,
-      required: [true, 'Provide zipcode'],
+      validate: [validator.isMobilePhone],
+    },
+    logo: String,
+    address: {
+      line_1: {
+        type: String,
+        required: [true, 'Please provide address line'],
+      },
+      line_2: String,
+      city: {
+        type: String,
+        required: [true, 'Please tell us the city'],
+      },
+      region: {
+        type: String,
+        required: [true, 'Provide the state/province/region'],
+      },
+      zipcode: {
+        type: String,
+        required: [true, 'Provide zipcode'],
+      },
+    },
+    location: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+    },
+    owner: {
+      type: Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Store must belong to an owner'],
     },
   },
-  location: {
-    // GeoJSON
-    type: {
-      type: String,
-      default: 'Point',
-      enum: ['Point'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
     },
-    coordinates: [Number],
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'Store must belong to an owner'],
-  },
+  }
+);
+
+// Virtual populate
+storeSchema.virtual('products', {
+  ref: 'Product',
+  foreignField: 'store',
+  localField: '_id',
 });
 
 const Store = model('Store', storeSchema);
