a8034c6788144bafa3aa77ce0cdd54113211b982
tchoforwilson <tchoforwilson@gmail.com>
2023-06-07 11:42:42 +0100

    Redefine model and included virtuals

--- models/user.model.js
+++ models/user.model.js
@@ -5,49 +5,67 @@ import validator from 'validator';
 
 import config from '../configurations/config.js';
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, 'Please tell us your user name'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide your email'],
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'user'],
-    default: 'user',
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide password'],
-    minlength: 8,
-    select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-    validate: {
-      // This only works on CREATE and SAVE!!!
-      validator: function (el) {
-        return el === this.password;
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please tell us your user name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same!',
       },
-      message: 'Passwords are not the same!',
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
     },
   },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active: {
-    type: Boolean,
-    default: true,
-    select: false,
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+  }
+);
+
+// Virtual populate
+userSchema.virtual('store', {
+  ref: 'Store',
+  foreignField: 'owner',
+  localField: '_id',
+  justOne: true,
 });
 
 /**
