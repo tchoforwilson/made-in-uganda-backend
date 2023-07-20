cceb939e99e29a7592fae7b06a729cfb5cb8e55e
tchoforwilson <tchoforwilson@gmail.com>
2023-07-20 11:15:45 +0100

    Refactored validation for product price and price discount using pre middleware functions

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,7 @@
-import { model, Schema } from 'mongoose';
+import mongoose from 'mongoose';
+import AppError from '../utilities/appError.js';
+
+const { Schema } = mongoose;
 
 const productSchema = new Schema(
   {
@@ -8,11 +11,11 @@ const productSchema = new Schema(
       trim: true,
       maxlength: [
         40,
-        'A product name must have less or equal then 40 characters',
+        'A product name must have less or equal than 40 characters',
       ],
       minlength: [
         5,
-        'A product name must have more or equal then 10 characters',
+        'A product name must have more or equal than 10 characters',
       ],
     },
     brand: String,
@@ -32,17 +35,13 @@ const productSchema = new Schema(
       },
       default: 'UGX',
     },
-    priceDiscount: {
+    priceDiscount: Number,
+    percentageDiscount: {
       type: Number,
-      validate: {
-        validator: function (val) {
-          // this only points to current doc on NEW document creation
-          return val < this.price;
-        },
-        message: 'Discount price ({VALUE}) should be below regular price',
+      set: function (value) {
+        return Math.round(value);
       },
     },
-    percentageDiscount: Number,
     imageCover: {
       type: String,
       required: [true, 'A product must have a cover photo'],
@@ -53,12 +52,12 @@ const productSchema = new Schema(
       trim: true,
     },
     category: {
-      type: Schema.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: 'Category',
       required: [true, 'A product must belong to a category'],
     },
     store: {
-      type: Schema.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: 'Store',
       required: [true, 'A product must belong to a store'],
     },
@@ -70,31 +69,69 @@ const productSchema = new Schema(
     },
   }
 );
+
 /**
- * @breif Middleware to calculate percentage discount before save
+ * @brief This middleware runs for update, to check for
+ * priceDiscount less than price
  */
-productSchema.pre('save', function (next) {
-  this.percentageDiscount = 100 - (this.priceDiscount * 100) / this.price;
+productSchema.pre('findOneAndUpdate', function (next) {
+  const update = this.getUpdate();
+  const price = update.price;
+  const priceDiscount = update.priceDiscount;
+
+  if (priceDiscount && price && priceDiscount >= price) {
+    return next(
+      new AppError('Discount price must be less than regular price', 400)
+    );
+  }
+
+  if (priceDiscount) {
+    update.$set = update.$set || {};
+    update.$set.percentageDiscount =
+      100 - (update.priceDiscount * 100) / update.price;
+  }
+
   next();
 });
 
 /**
- * @breif Populate product with store when find
+ * @brief This middleware runs for save and create, to check for
+ * priceDiscount less than price
+ */
+productSchema.pre('save', function (next) {
+  if (this.priceDiscount && this.priceDiscount >= this.price) {
+    return next(
+      new AppError('Discount price must be less than regular price', 400)
+    );
+  }
+
+  if (this.priceDiscount) {
+    this.percentageDiscount =
+      Math.round(((this.price - this.priceDiscount) / this.price) * 10000) /
+      100;
+  } else {
+    this.percentageDiscount = 0;
+  }
+
+  next();
+});
+
+/**
+ * @breif Populate middleware in find operation for
+ * store and category
  */
 productSchema.pre(/^find/, function (next) {
-  // Populate with store
   this.populate({
     path: 'store',
     select: 'name _id',
-  });
-  // Populate with category
-  this.populate({
+  }).populate({
     path: 'category',
     select: 'name _id',
   });
+
   next();
 });
 
-const Product = model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 export default Product;
