82b588e75b2164821ab460e7005d39c5190df4a6
tchoforwilson <tchoforwilson@gmail.com>
2023-09-08 10:23:38 +0100

    Removed all authentication routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,15 +6,9 @@ import eUserRole from '../utilities/enums/e.user-role.js';
 
 const router = Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword/:token', authController.resetPassword);
-
 // PROTECT ALL ROUTE AFTER THIS MIDDLEWARE
 router.use(authController.protect);
 
-router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
 router.patch('/updateMe', userController.updateMe);
 router.patch('/deleteMe', userController.deleteMe);
