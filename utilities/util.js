9723117493878e66006da286b364278552b28a6b
tchoforwilson <tchoforwilson@gmail.com>
2023-09-10 08:23:42 +0100

    Implemented method subscriptionIsExpired

--- utilities/util.js
+++ utilities/util.js
@@ -1,3 +1,5 @@
+import { MONTHLY_SUBCRIPTIONS_TIME } from './constants/index.js';
+
 /**
  * @breif Filter out unwanted fields in an object
  * @param {Object} obj -> Provided object
@@ -10,4 +12,17 @@ export const filterObj = (obj, ...allowedFields) => {
     if (allowedFields.includes(el)) newObj[el] = obj[el];
   });
   return newObj;
+};
+
+/**
+ * @breif Check if user monthly subscription has expired after 30 days.
+ * @param {String} paymentDate Last payment date
+ * @returns {Boolean}
+ */
+export const subcriptionIsExpired = (paymentDate) => {
+  if (!paymentDate) return true;
+  const currentDate = new Date();
+  const dateToCheck = new Date(paymentDate);
+  const timeDiffInMs = currentDate.getTime() - dateToCheck.getTime();
+  return timeDiffInMs > MONTHLY_SUBCRIPTIONS_TIME;
 };
