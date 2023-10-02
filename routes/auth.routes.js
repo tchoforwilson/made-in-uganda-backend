e87c083e390f3f666dc8f76f8ee591b4a03b10ba
tchoforwilson <tchoforwilson@gmail.com>
2023-10-02 06:49:00 +0100

    Route for refresh token

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,7 +8,7 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword/:token', authController.resetPassword);
-router.post('/refresh-toekn', authController.refreshToken);
+router.post('/refresh', authController.refreshToken);
 
 // Protect this authentication routes
 router.use(authController.protect);
